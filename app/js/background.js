4298eb80ac400d90e419b95784df9df14d9f14ae
dtaniwaki <daisuketaniwaki@gmail.com>
2014-05-20 09:01:37 +0900

    Update the badge when the count is zero

--- app/js/background.js
+++ app/js/background.js
@@ -8,6 +8,8 @@ $(function(){
       if (num > 0) {
         chrome.browserAction.setBadgeText({text: String(num)})
         chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 200]})
+      } else {
+        chrome.browserAction.setBadgeText({text: ""})
       }
     });
   };
